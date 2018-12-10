27fbb0bdc6964175c4b948553797d0c3073c726b
zeting <zeting@vipcare.com>
2018-12-10 19:39:31 +0800

    提交动画

--- app/src/main/java/frame/zzt/com/appframe/slidelock/ActivitySlideLock.java
+++ app/src/main/java/frame/zzt/com/appframe/slidelock/ActivitySlideLock.java
@@ -1,9 +1,21 @@
 package frame.zzt.com.appframe.slidelock;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import frame.zzt.com.appframe.R;
+import frame.zzt.com.appframe.UI.BaseAppCompatActivity;
+
 /**
  * Created by zeting
  * Date 18/12/10.
  */
 
-public class ActivitySlideLock {
+public class ActivitySlideLock extends BaseAppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_lock);
+    }
 }
