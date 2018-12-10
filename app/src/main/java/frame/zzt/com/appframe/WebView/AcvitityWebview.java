8c50893c8714af37762da4f5288cfa9354f84926
zeting <zeting@vipcare.com>
2018-12-10 19:38:35 +0800

    提交滑动按钮动画

---
+++ app/src/main/java/frame/zzt/com/appframe/WebView/AcvitityWebview.java
@@ -0,0 +1,9 @@
+package frame.zzt.com.appframe.WebView;
+
+/**
+ * Created by zeting
+ * Date 18/12/10.
+ */
+
+public class AcvitityWebview {
+}
---
+++ app/src/main/java/frame/zzt/com/appframe/slidelock/ActivitySlideLock.java
@@ -0,0 +1,9 @@
+package frame.zzt.com.appframe.slidelock;
+
+/**
+ * Created by zeting
+ * Date 18/12/10.
+ */
+
+public class ActivitySlideLock {
+}
