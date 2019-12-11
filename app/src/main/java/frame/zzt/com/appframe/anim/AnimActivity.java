9c145b6be7a630e79ec89359cf72aeccafb3f1e2
zhu_zeting <zhu_zeting@163.com>
2019-12-11 22:50:17 +0800

    提交动画

--- app/src/main/java/frame/zzt/com/appframe/anim/AnimActivity.java
+++ app/src/main/java/frame/zzt/com/appframe/anim/AnimActivity.java
@@ -1,5 +1,12 @@
 package frame.zzt.com.appframe.anim;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.Keyframe;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
 import android.app.Activity;
 import android.app.ActivityOptions;
 import android.content.Intent;
@@ -17,6 +24,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.CycleInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -40,30 +49,35 @@ import frame.zzt.com.appframe.mvp.mvpbase.BaseActivity;
 
 public class AnimActivity extends BaseAppCompatActivity {
 
-    private static final String TAG = AnimActivity.class.getSimpleName() ;
+    private static final String TAG = AnimActivity.class.getSimpleName();
 
 
     @BindView(R.id.tv_anim_msg)
-    TextView tv_anim_msg ;
+    TextView tv_anim_msg;
 
     @BindView(R.id.btn_anim_start)
-    Button btn_anim_start ;
+    Button btn_anim_start;
 
     @BindView(R.id.iv_yx)
-    ImageView iv_yx ;
+    ImageView iv_yx;
 
     @BindView(R.id.spinner_anim)
-    Spinner spinner_anim ;
+    Spinner spinner_anim;
 
     @BindView(R.id.btn_01)
-    Button btn_01 ;
+    Button btn_01;
     @BindView(R.id.btn_02)
-    Button btn_02 ;
+    Button btn_02;
     @BindView(R.id.btn_03)
-    Button btn_03 ;
+    Button btn_03;
+    @BindView(R.id.btn_04)
+    Button btn_04;
+    @BindView(R.id.btn_05)
+    Button btn_05;
+
 
     // 选择的数据id
-    private int selectPosition = 0 ;
+    private int selectPosition = 0;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -80,7 +94,7 @@ public class AnimActivity extends BaseAppCompatActivity {
             // 4.0 添加
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             // 5.0 添加
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             //注意要清除 FLAG_TRANSLUCENT_STATUS flag
@@ -94,10 +108,9 @@ public class AnimActivity extends BaseAppCompatActivity {
 //            注意要清除 FLAG_TRANSLUCENT_STATUS flag
             getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             // 6.0 设置状态栏颜色为深色
-            getWindow().getDecorView().setSystemUiVisibility( View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR );
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
-
 
 
         btn_01.setOnClickListener(new View.OnClickListener() {
@@ -127,7 +140,20 @@ public class AnimActivity extends BaseAppCompatActivity {
             }
         });
 
-   }
+        btn_04.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNoviceAnim(v);
+            }
+        });
+
+        btn_05.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnim(v);
+            }
+        });
+    }
 
 //    public  void showBar(Activity activity){
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -153,7 +179,7 @@ public class AnimActivity extends BaseAppCompatActivity {
 //    }
 
 
-            private void initView() {
+    private void initView() {
         btn_anim_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -164,14 +190,14 @@ public class AnimActivity extends BaseAppCompatActivity {
         iv_yx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( selectPosition == 0 ){
+                if (selectPosition == 0) {
                     /**
                      * 和overridePendingTransition类似,设置跳转时候的进入动画和退出动画
                      */
                     ActivityOptionsCompat compat1 = ActivityOptionsCompat.makeCustomAnimation(AnimActivity.this,
                             R.anim.slide_right_in, R.anim.slide_left_out);
-                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this,  AnimShowActivity.class), compat1.toBundle());
-                }else if ( selectPosition == 1 ){
+                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this, AnimShowActivity.class), compat1.toBundle());
+                } else if (selectPosition == 1) {
                     /**
                      * 通过把要进入的Activity通过放大的效果过渡进去
                      * 举一个简单的例子来理解source=view,startX=view.getWidth(),startY=view.getHeight(),startWidth=0,startHeight=0
@@ -179,23 +205,23 @@ public class AnimActivity extends BaseAppCompatActivity {
                      */
                     ActivityOptionsCompat compat2 = ActivityOptionsCompat.makeScaleUpAnimation(v,
                             v.getWidth() / 2, v.getHeight() / 2, 0, 0);
-                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this,  AnimShowActivity.class), compat2.toBundle());
-                }else if ( selectPosition == 2 ){
+                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this, AnimShowActivity.class), compat2.toBundle());
+                } else if (selectPosition == 2) {
                     /**
                      * 通过放大一个图片过渡到新的Activity
                      */
                     v.setDrawingCacheEnabled(true);
-                    Bitmap bitmap=v.getDrawingCache();
-                    ActivityOptionsCompat compat3 = ActivityOptionsCompat.makeThumbnailScaleUpAnimation(v,bitmap, 0, 0);
-                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this,  AnimShowActivity.class), compat3.toBundle());
-                }else if ( selectPosition == 3 ){
+                    Bitmap bitmap = v.getDrawingCache();
+                    ActivityOptionsCompat compat3 = ActivityOptionsCompat.makeThumbnailScaleUpAnimation(v, bitmap, 0, 0);
+                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this, AnimShowActivity.class), compat3.toBundle());
+                } else if (selectPosition == 3) {
                     /**
                      * 场景动画，体现在两个Activity中的某些view协同去完成过渡动画效果
                      */
                     ActivityOptionsCompat compat4 =
-                            ActivityOptionsCompat.makeSceneTransitionAnimation(AnimActivity.this,  v , getString(R.string.show_scene_transition_anim));
-                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this,  AnimShowActivity.class), compat4.toBundle());
-                }else if ( selectPosition == 4 ){
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(AnimActivity.this, v, getString(R.string.show_scene_transition_anim));
+                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this, AnimShowActivity.class), compat4.toBundle());
+                } else if (selectPosition == 4) {
                     /**
                      * 场景动画，同上是对多个View同时起作用
                      */
@@ -204,14 +230,14 @@ public class AnimActivity extends BaseAppCompatActivity {
                     Pair<View, String> textPair = Pair.create((View) tv_anim_msg, getString(R.string.show_scene_transition_anim_title));
 
                     ActivityOptionsCompat compat5 =
-                            ActivityOptionsCompat.makeSceneTransitionAnimation(AnimActivity.this, imagePair,textPair );
-                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this,  AnimShowActivity.class), compat5.toBundle());
-                }else {
+                            ActivityOptionsCompat.makeSceneTransitionAnimation(AnimActivity.this, imagePair, textPair);
+                    ActivityCompat.startActivity(AnimActivity.this, new Intent(AnimActivity.this, AnimShowActivity.class), compat5.toBundle());
+                } else {
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                         Intent mIntent = new Intent();
-                        mIntent.setClass(AnimActivity.this , AnimShowActivity.class ) ;
-                        mIntent.putExtra( "index" , selectPosition );
-                        startActivity(mIntent , ActivityOptions.makeSceneTransitionAnimation(AnimActivity.this).toBundle());
+                        mIntent.setClass(AnimActivity.this, AnimShowActivity.class);
+                        mIntent.putExtra("index", selectPosition);
+                        startActivity(mIntent, ActivityOptions.makeSceneTransitionAnimation(AnimActivity.this).toBundle());
                     }
                 }
             }
@@ -224,29 +250,125 @@ public class AnimActivity extends BaseAppCompatActivity {
         list.add("4 - makeSceneTransitionAnimation");
         list.add("5 - makeSceneTransitionAnimation");
         list.add("6 - 自己转换动画");
-        ArrayAdapter adapter2 = new ArrayAdapter(this, R.layout.item_show_anim, R.id.tv_item_show,list);
+        ArrayAdapter adapter2 = new ArrayAdapter(this, R.layout.item_show_anim, R.id.tv_item_show, list);
         spinner_anim.setAdapter(adapter2);
         spinner_anim.setOnItemSelectedListener(new spinnerListener());
 
 
-
-
-
-
-
     }
 
-    class spinnerListener implements android.widget.AdapterView.OnItemSelectedListener{
+    class spinnerListener implements android.widget.AdapterView.OnItemSelectedListener {
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            Log.i(TAG , "选择数据" + position );
-            selectPosition = position ;
+            Log.i(TAG, "选择数据" + position);
+            selectPosition = position;
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
-            Log.i(TAG , "onNothingSelected" );
+            Log.i(TAG, "onNothingSelected");
+        }
+    }
+
+
+    AnimatorSet set;
+
+    /**
+     * 设置新手券动画
+     */
+    public void startNoviceAnim(View view) {
+
+        ObjectAnimator anim1 = ObjectAnimator.ofFloat(view, "translationX", 0, -20);
+        anim1.setDuration(1000);
+        anim1.setInterpolator(new CycleInterpolator(1));
+
+        ObjectAnimator anim2 = ObjectAnimator.ofFloat(view, "translationX", 0, -10);
+        anim2.setDuration(1000);
+        anim2.setInterpolator(new CycleInterpolator(1));
+
+        set = new AnimatorSet();
+        set.play(anim1).before(anim2);
+        set.start();
+        set.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                set.setStartDelay(1500);
+                set.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+
+    }
+
+
+    ObjectAnimator nopeAnimator;
+
+    /**
+     * 设置平移动画
+     * @param view
+     * @param duration
+     * @param distance
+     * @return
+     */
+    public ObjectAnimator createAnim(View view , Long duration ,  int distance) {
+        PropertyValuesHolder pvhTranslateX = PropertyValuesHolder.ofKeyframe(View.TRANSLATION_X,
+                Keyframe.ofFloat(0f, 0),
+                Keyframe.ofFloat(0.125f, -distance),
+                Keyframe.ofFloat(0.25f, 0),
+                Keyframe.ofFloat(0.375f, distance),
+                Keyframe.ofFloat(0.5f, 0),
+
+                Keyframe.ofFloat(0.625f, -distance * 0.6f),
+                Keyframe.ofFloat(0.75f, 0),
+                Keyframe.ofFloat(0.875f, distance * 0.6f),
+                Keyframe.ofFloat(1f, 0f)
+        );
+
+        return ObjectAnimator.ofPropertyValuesHolder(view, pvhTranslateX).setDuration(duration);
+    }
+
+    /**
+     * 启动动画
+     *
+     * @param view
+     */
+    public void startAnim(View view) {
+        if (nopeAnimator == null) {
+            nopeAnimator = createAnim(view , 1000L , 10);
+        }
+        nopeAnimator.setInterpolator(new LinearInterpolator());
+        nopeAnimator.start();
+        nopeAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                animation.setStartDelay(2000);
+                animation.start();
+            }
+        });
+    }
+
+    /**
+     * 清除动画
+     */
+    public void stopAnim() {
+        if (nopeAnimator != null && nopeAnimator.isRunning()) {
+            nopeAnimator.cancel();
+            nopeAnimator = null;
         }
     }
 
